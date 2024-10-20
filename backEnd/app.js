acd7737bf8c6dee8a3cd1cdc84cf2eb6778183cb
Siddhartha Mukhejee <siddharthamukherjee0709@gmail.com>
2024-10-20 14:34:38 +0530

    express public

--- backEnd/app.js
+++ backEnd/app.js
@@ -7,8 +7,9 @@ import postRouter from "./routes/post.routes.js";
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
